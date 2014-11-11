83b9bbb80646ed399b45f29ef2670deb04f45958
Matt Feeney <mjfeeney3@gmail.com>
2014-11-11 10:50:53 -0500

    WIP
    11/11 first commit

---
+++ craft/app/Info.php
@@ -0,0 +1,10 @@
+<?php
+namespace Craft;
+
+define('CRAFT_VERSION', '2.2');
+define('CRAFT_BUILD', '2581');
+define('CRAFT_SCHEMA_VERSION', '2.2.0');
+define('CRAFT_RELEASE_DATE', '1409864651');
+define('CRAFT_MIN_BUILD_REQUIRED', '2570');
+define('CRAFT_MIN_BUILD_URL', 'http://download.buildwithcraft.com/craft/2.1/2.1.2570/Craft-2.1.2570.zip');
+define('CRAFT_TRACK', 'stable');
